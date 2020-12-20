755382ad08bf8bb1bffb6ab8989022a90eeef22c
SamirElKharrat <samirelkharrat@gmail.com>
2020-12-20 13:27:03 +0000

    Empezamos el controller de location

--- src/main/java/dad/geofx/samir/controller/LocationController.java
+++ src/main/java/dad/geofx/samir/controller/LocationController.java
@@ -1,5 +1,86 @@
 package dad.geofx.samir.controller;
 
-public class LocationController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import dad.geofx.samir.model.Location;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class LocationController implements Initializable{
+
+	//controller
+	private ObjectProperty<Location> location = new SimpleObjectProperty();
+	
+	//view
+	@FXML
+    private GridPane view;
+
+    @FXML
+    private Label latitudL;
+
+    @FXML
+    private Label longitudL;
+
+    @FXML
+    private Label ipL;
+
+    @FXML
+    private ImageView paisesI;
+
+    @FXML
+    private Label ciudadL;
+
+    @FXML
+    private Label zipL;
+
+    @FXML
+    private Label zonaL;
+
+    @FXML
+    private Label currencyL;
+
+    @FXML
+    private Label lenguaL;
+
+    @FXML
+    private Label codeL;
+
+	public LocationController() throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/LocationView.fxml"));
+		loader.setController(this);
+		loader.load();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+	
+	public GridPane getView() {
+		return view;
+	}
+
+	public final ObjectProperty<Location> locationProperty() {
+		return this.location;
+	}
+	
+
+	public final Location getLocation() {
+		return this.locationProperty().get();
+	}
+	
+
+	public final void setLocation(final Location location) {
+		this.locationProperty().set(location);
+	}
+	
 
 }
